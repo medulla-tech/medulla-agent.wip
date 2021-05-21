28425dde057c81f6b13964db2665c0809ed22422
jfkneib <jean-francois.kneib@siveo.net>
2021-05-21 11:33:52 +0200

    [fixe registration] les ars nepouve plus s'enregistre
    [feature ars] check status ON/OFF ars (send monitoring message, send message historique)

--- pulse_xmpp_master_substitute/lib/plugins/xmpp/__init__.py
+++ pulse_xmpp_master_substitute/lib/plugins/xmpp/__init__.py
@@ -1679,7 +1679,8 @@ class XmppMasterDatabase(DatabaseHelper):
                     session.execute(sql)
                     session.commit()
                     session.flush()
-                self.checknewjid(jid)
+                else:
+                    self.checknewjid(jid)
             except Exception, e:
                 logging.getLogger().error(str(e))
                 msg=str(e)
@@ -5107,6 +5108,60 @@ class XmppMasterDatabase(DatabaseHelper):
             return a
         except:
             return -1
+
+    @DatabaseHelper._sessionm
+    def update_Presence_Relay(self, session, jid, presense=0):
+        """
+            update relay table presense
+        """
+        try:
+            user = str(jid).split("@")[0]
+
+            sql = """UPDATE
+                        `xmppmaster`.`machines`
+                    SET
+                        `enabled` = '%s'
+                    WHERE
+                        `xmppmaster`.`machines`.`jid` like('%s@%%');""" % (presense,
+                                                                           user)
+            session.execute(sql)
+            sql = """UPDATE
+                        `xmppmaster`.`relayserver`
+                    SET
+                        `enabled` = '%s'
+                    WHERE
+                        `xmppmaster`.`relayserver`.`jid` like('%s@%%');""" % (presense,
+                                                                              user)
+            session.execute(sql)
+            session.commit()
+            session.flush()
+        except Exception, e:
+            logging.getLogger().error(str(e))
+            logging.getLogger().error("\n%s" % (traceback.format_exc()))
+
+    @DatabaseHelper._sessionm
+    def update_reconf_mach_of_Relay_down(self, session, jid, reconf=1):
+        """
+            renitialise remote configuration
+        """
+        try:
+            user = str(jid).split("@")[0]
+            sql = """UPDATE
+                        `xmppmaster`.`machines`
+                     SET
+                        `need_reconf` = '%s'
+                     WHERE
+                        `xmppmaster`.`machines`.`agenttype` like ("machine")
+                        AND
+                        `xmppmaster`.`machines`.`groupdeploy` like('%s@%%');""" % (reconf,
+                                                                           user)
+            session.execute(sql)
+            session.commit()
+            session.flush()
+        except Exception, e:
+            logging.getLogger().error(str(e))
+            logging.getLogger().error("\n%s" % (traceback.format_exc()))
+
 
     @DatabaseHelper._sessionm
     def delPresenceMachine(self, session, jid):
