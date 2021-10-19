ac59d997278875bfe8db935d1e65d6f4fa19d31c
Nicolas Lécureuil <neoclust@mageia.org>
2021-10-19 15:58:39 +0200

    (rescue agent) Add the Rescue agent.
    When the agent starts it creates a rescue agent with the copy of the
    working agent.
    
    When the agent does not starts or if it enters into a loop, we reinstall
    the agent with the "rescue image".
    
    This allow to fix broken updates, etc.
    
    We creates a BOOL_FILE_CONTROL_WATCH_DOG watchdog file, to ensure that the
    pulse agent runs smoothly

--- pulse_xmpp_agent/agentxmpp.py
+++ pulse_xmpp_agent/agentxmpp.py
@@ -404,6 +404,13 @@ class MUCBot(sleekxmpp.ClientXMPP):
             # As long as the Relayserver Agent isn't started, the sesion queues
             # where the deploy has failed are not useful
             self.session.clearallfilesession()
+
+        if self.config.agenttype in ['machine']:
+            self.schedule('stabilized_start',
+                          120,
+                          self.stabilized_start,
+                          repeat=True)
+
         self.reversessh = None
         self.reversesshmanage = {}
         self.signalinfo = {}
@@ -600,6 +607,22 @@ class MUCBot(sleekxmpp.ClientXMPP):
                         15,
                         self.initialise_syncthing,
                         repeat=False)
+
+    def stabilized_start(self):
+        """
+            It creates a file called BOOL_FILE_CONTROL_WATCH_DOG with
+            inside the pid and a date when it has been created.
+            It is used to see if the program runs correctly.
+        """
+        directory_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                                  "INFOSTMP")
+        BOOL_FILE_CONTROL_WATCH_DOG = os.path.join(directory_file,
+                                                   "BOOL_FILE_CONTROL_WATCH_DOG")
+
+        file_put_contents(BOOL_FILE_CONTROL_WATCH_DOG,
+                              "process %s :(%s)" % (os.getpid(), str(datetime.now())))
+
+        logger.debug("creation BOOL_FILE_CONTROL_WATCH_DOG in %s" % BOOL_FILE_CONTROL_WATCH_DOG)
 
     def QDeployfile(self):
         sessioniddata = getRandomName(6, "Qdeployfile")
